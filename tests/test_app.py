c457e1a95c5677d649bbd4d42cc03d8c3f14e14b
Diego Alejandro Robledo <68504016+alexriley7@users.noreply.github.com>
2025-01-20 12:07:44 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,6 @@ import pytest
 from src.app import add
 
 def test_add():
-    assert add(2, 3) == 555
+    assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(0, 0) == 00
+    assert add(0, 0) == 0
